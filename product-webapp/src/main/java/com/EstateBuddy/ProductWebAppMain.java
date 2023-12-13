3672ed467c001b9e5fde01db326435201d6cd855
Roopesh Rokade <04012001roopesh@gmail.com>
2023-12-13 21:15:05 +0530

    Initial setup

---
+++ product-webapp/src/main/java/com/EstateBuddy/ProductWebAppMain.java
@@ -0,0 +1,11 @@
+package com.EstateBuddy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProductWebAppMain {
+    public static void main(String[] args) {
+        SpringApplication.run(ProductWebAppMain.class,args);
+    }
+}
